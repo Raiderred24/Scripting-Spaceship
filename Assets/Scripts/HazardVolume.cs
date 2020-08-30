a751d7d2c318f953621a52c1974619c186f5edc2
Hayden <haydenhurst64@gmail.com>
2020-08-30 18:28:31 -0500

    Setup (24) Complete

---
+++ Assets/Scripts/HazardVolume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardVolume : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Detect if it's the player
+        PlayerShip playerShip
+            = other.gameObject.GetComponent<PlayerShip>();
+        // If we fodun something valid then continue
+        if (playerShip != null)
+        {
+            // Death
+            playerShip.Kill();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerShip : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 12f;
+    [SerializeField] float _turnSpeed = 3f;
+
+    Rigidbody _rb = null;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveShip();
+        TurnShip();
+    }
+    void MoveShip()
+    {
+        // S/Down = -1, W/Up = 1, None = 0. Scale by moveSpeed
+        float moveAmountThisFrame = Input.GetAxisRaw("Vertical") * _moveSpeed;
+        // Combine our direction with our calculated amount
+        Vector3 moveDirection = transform.forward * moveAmountThisFrame;
+        _rb.AddForce(moveDirection);
+    }
+    void TurnShip()
+    {
+        // A/Left = -1, D/Right = 1, None = 0. Scale by turnSpeed
+        float turnAmountThisFrame = Input.GetAxisRaw("Horizontal") * _turnSpeed;
+        // Specify an axis to apply our turn amount (x,y,z) as a rotation
+        Quaternion turnOffset = Quaternion.Euler(0, turnAmountThisFrame, 0);
+        // Spin the Ship
+        _rb.MoveRotation(_rb.rotation * turnOffset);
+    }
+    public void Kill()
+    {
+        Debug.Log("Player has died!");
+        this.gameObject.SetActive(false);
+    }
+}
